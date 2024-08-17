b9d85600a90c28aaea058886ebf2179c7cbb625b
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2024-08-16 23:03:14 -0400

    style: add nicer mobile footer

--- frontend/src/components/footer/index.jsx
+++ frontend/src/components/footer/index.jsx
@@ -1,9 +1,18 @@
 import "./style.css";
+import TipverseLogo from "img/TipverseLogo.png";
 function Footer() {
   return (
+    
     <footer className="footer">
-      <p className="footer__copyright">Copyright &copy; 2024</p>
-      <p className="footer__author">Author: @clickwithclark</p>
+      <div className="footer__wrapper">
+      <figure className="footer__logo">
+        <img src={TipverseLogo} alt="Tipverse Logo"  />
+      </figure>
+        <div className="footer__details">
+          <p className="footer__copyright">Copyright &copy; 2024</p>
+          <p className="footer__author">Author: @clickwithclark</p>
+        </div>
+      </div>
     </footer>
   );
 }
