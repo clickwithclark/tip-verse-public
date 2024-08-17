d2c607d244891292f6347b1a70f0236ebfec068c
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2024-08-17 02:44:57 -0400

    feat: add link to github repo

--- frontend/src/components/footer/index.jsx
+++ frontend/src/components/footer/index.jsx
@@ -2,12 +2,17 @@ import "./style.css";
 import TipverseLogo from "img/TipverseLogo.png";
 function Footer() {
   return (
-    
     <footer className="footer">
       <div className="footer__wrapper">
-      <figure className="footer__logo">
-        <img src={TipverseLogo} alt="Tipverse Logo"  />
-      </figure>
+        <figure className="footer__logo">
+          <img src={TipverseLogo} alt="Tipverse Logo" />
+        </figure>
+        <div className="footer__github-log">
+          <a href="https://github.com/clickwithclark/tip-verse-public">
+            {" "}
+            <img src="/src/assets/img/gh_logo.png" alt="github icon" />
+          </a>
+        </div>
         <div className="footer__details">
           <p className="footer__copyright">Copyright &copy; 2024</p>
           <p className="footer__author">Author: @clickwithclark</p>
